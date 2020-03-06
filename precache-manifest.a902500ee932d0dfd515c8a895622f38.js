0646d1f9508c19714d0aaa36c3a45c6b46ca89ed
Betrayer <StarDestroyer@ukr.net>
2020-03-06 16:25:54 +0200

    Updates

---
+++ precache-manifest.a902500ee932d0dfd515c8a895622f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed6f3c389b9f6aa471972bfe00d6705",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "3c9739dbffe781ffcb2a",
+    "url": "/goit-react-hw-01-components/static/css/main.5ea5db76.chunk.css"
+  },
+  {
+    "revision": "3e5c718bfacae433874e",
+    "url": "/goit-react-hw-01-components/static/js/2.f69a81cc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.f69a81cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9739dbffe781ffcb2a",
+    "url": "/goit-react-hw-01-components/static/js/main.00da8b37.chunk.js"
+  },
+  {
+    "revision": "d1223a56ce2b31e92e84",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.687648de.js"
+  }
+]);
